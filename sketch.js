ea83e5b5bb859fc6765fa04fef695baf76f6770e
Rayeed-Hasan <rayeedth@gmail.com>
2021-02-19 20:23:15 +0600

    sample test

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+console.log("rayeed");
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(180, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -62,3 +62,8 @@ function draw(){
     bird.display();
     platform.display();
 }
+
+
+
+
+
